a7416431a4c814b750e2d0a0a171e5a6228b2238
FernandoHuilca <fernando.huilca@epn.edu.ec>
2024-07-06 12:49:12 -0500

    Ya acabé todo funcional

--- arboles-avl/src/Main.java
+++ arboles-avl/src/Main.java
@@ -9,17 +9,17 @@ public class Main {
             opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
                     " _______________ ELIJA UNA OPERACIÓN ÁRBOLES AVL _______________ \n\n" +
 
-                            "                                              1)  CREACIÓN DEL ÁRBOL AVL \n\n" +
+                            "                                     1)  CREACIÓN DEL ÁRBOL AVL \n\n" +
 
-                            "                                              2)  BUSCAR DENTRO DEL ÁRBOL\n\n" +
+                            "                                     2)  BUSCAR DENTRO DEL ÁRBOL\n\n" +
 
-                            "                                              3)  INSERTAR EN EL ÁRBOL\n\n" +
+                            "                                     3)  INSERTAR EN EL ÁRBOL\n\n" +
 
-                            "                                              4)  ELIMINAR ALGÚN ELEMENTO\n\n" +
+                            "                                     4)  ELIMINAR ALGÚN ELEMENTO\n\n" +
 
-                            "                                              5)  VER ÁRBOL AVL\n\n" +
+                            "                                     5)  VER ÁRBOL AVL\n\n" +
 
-                            "                                              0)  SALIR\n\n"));
+                            "                                     0)  SALIR\n\n"));
             switch (opcion) {
                 case 1:
                     arbolAVL.crearÁrbol();
@@ -46,7 +46,7 @@ public class Main {
                     JOptionPane.showMessageDialog(null, "FIN DEL PROGRAMA, gracias por usarlo :D.");
                     break;
                 default:
-                    JOptionPane.showMessageDialog(null, "ERROR DESCONOCIDO.");
+                    JOptionPane.showMessageDialog(null, "ERROR. Elija una opción de las que se presentan. Pilas.");
                     break;
             }
         } while (opcion != 0);
